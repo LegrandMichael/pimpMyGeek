e49aa98d82886ff4366527f0b61fc27a28d3272b
LegrandMichael <michael.c.legrand@gmail.com>
2018-05-03 17:57:42 +0200

    Project Init

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'DB_PMG',
+    'database_user' => 'root',
+    'database_password' => 'admin',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '9swBxQALHdBbcPoa1L0pOEFwuYZ5Jvqad72sJYTqkTosEcqBoFU6uWVz',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-05-03',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'Y9Gyr5FFdI4hTh2bZ18XK597HBIXA2pHNE4UUDHUu7a9JyOq0eQeOUQA',
+    'cookie_iv' => 'EttCHMS4',
+    'new_cookie_key' => 'def000004f244f45e6f5953ed70e49cb08c0cc94b9a3191e6805aab4ac610826db77ad57434cf1af0eb85a9ef106db74ce773bf4ec57d42d87cb7f128801603c5d5428fc',
+  ),
+);
